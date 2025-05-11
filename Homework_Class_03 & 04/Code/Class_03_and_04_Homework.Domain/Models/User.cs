cd13781566f6d679fb76c6a4bc9ba67fe2b10922
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-12 00:38:06 +0800

    Add project files.

---
+++ Homework_Class_03 & 04/Code/Class_03_and_04_Homework.Domain/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_03_and_04_Homework.Domain.Models
+{
+    class User
+    {
+    }
+}
---
+++ Homework_Class_03 & 04/Code/Class_03_and_04_Homework.Domain/Models/Vehicle.cs
@@ -0,0 +1,6 @@
+﻿namespace Class_03_and_04_Homework.Domain.Models
+{
+    class Vehicle
+    {
+    }
+}
